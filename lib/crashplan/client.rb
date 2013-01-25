afca407788a127b2364302dbbd87e536440bfa10
Allan Tokuda <allan.tokuda@code42.com>
2013-01-25 14:27:17 -0600

    Testing for presence of required settings

--- lib/crashplan/client.rb
+++ lib/crashplan/client.rb
@@ -1,10 +1,17 @@
 module Crashplan
   class Client
+    attr_accessor :host, :port
     def user
+      check_settings
       "my user"
     end
     def org
+      check_settings
       "my org"
+    end
+    def check_settings
+      raise "Host is not set" if host.nil?
+      raise "Port is not set" if port.nil?
     end
   end
 end
--- spec/crashplan/client_spec.rb
+++ spec/crashplan/client_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 
 describe Crashplan::Client do
-  #before do
-  #  subject.host = 'example.com'
-  #  subject.port = 1234
-  #end
+  before do
+    subject.host = 'example.com'
+    subject.port = 1234
+  end
   context "when given valid parameters" do
+    it "should return configured host" do
+      subject.host.should == "example.com"
+    end
+    it "should return configured port" do
+      subject.port.should == 1234
+    end
+  end
+
+  describe "user" do
     it "should return my user" do
       subject.user.should == "my user"
     end
+    it "should throw an error when missing host" do
+      subject.host = nil
+      expect { subject.user }.to raise_error
+    end
+    it "should throw an error when missing port" do
+      subject.port = nil
+      expect { subject.user }.to raise_error
+    end
+  end
+    
+  describe "org" do
     it "should return my org" do
       subject.org.should == "my org"
     end
-  end
-  context "when given invalid parameters" do
-    it "should do something else"
+    it "should throw an error when missing host" do
+      subject.host = nil
+      expect { subject.org }.to raise_error
+    end
+    it "should throw an error when missing port" do
+      subject.port = nil
+      expect { subject.org }.to raise_error
+    end
   end
 end

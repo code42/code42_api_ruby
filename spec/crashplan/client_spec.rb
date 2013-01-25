31df70301812245e208cfbd146551aad476f8c46
Nick Colgan <nick.colgan@code42.com>
2013-01-25 14:47:13 -0600

    Refactor spec

--- spec/crashplan/client_spec.rb
+++ spec/crashplan/client_spec.rb
@@ -1,44 +1,50 @@
 require 'spec_helper'
 
 describe Crashplan::Client do
+  subject(:client) { Crashplan::Client.new }
+
   before do
-    subject.host = 'example.com'
-    subject.port = 1234
+    client.host = 'example.com'
+    client.port = 1234
   end
-  context "when given valid parameters" do
-    it "should return configured host" do
-      subject.host.should == "example.com"
-    end
-    it "should return configured port" do
-      subject.port.should == 1234
-    end
+
+  it "should return configured host" do
+    client.host.should == "example.com"
+  end
+
+  it "should return configured port" do
+    client.port.should == 1234
   end
 
   describe "user" do
     it "should return my user" do
-      subject.user.should == "my user"
+      client.user.should == "my user"
     end
+
     it "should throw an error when missing host" do
-      subject.host = nil
-      expect { subject.user }.to raise_error
+      client.host = nil
+      expect { client.user }.to raise_error
     end
+
     it "should throw an error when missing port" do
-      subject.port = nil
-      expect { subject.user }.to raise_error
+      client.port = nil
+      expect { client.user }.to raise_error
     end
   end
     
   describe "org" do
     it "should return my org" do
-      subject.org.should == "my org"
+      client.org.should == "my org"
     end
+
     it "should throw an error when missing host" do
-      subject.host = nil
-      expect { subject.org }.to raise_error
+      client.host = nil
+      expect { client.org }.to raise_error
     end
+
     it "should throw an error when missing port" do
-      subject.port = nil
-      expect { subject.org }.to raise_error
+      client.port = nil
+      expect { client.org }.to raise_error
     end
   end
 end

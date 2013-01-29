ba82a64660bf48c056985450f8b3a3c6cb1fdaf3
Nick Colgan <nick.colgan@code42.com>
2013-01-29 09:58:23 -0600

    Clean up specs

--- spec/crashplan/client_spec.rb
+++ spec/crashplan/client_spec.rb
@@ -26,36 +26,35 @@ describe Crashplan::Client do
   describe "#user" do
     context "when ID is not passed" do
       it "should return my user" do
-        stub_get(/example.com/).to_return(body: fixture('user.my.json'))
-        expect(client.user).to be_a Crashplan::User
+        stub_get(%r{/user/my}).to_return(body: fixture('user.my.json'))
+        user = client.user
+        expect(user.id).to eq 1
       end
     end
 
     context "when ID is passed in" do
       it "should return specific user" do
-        request = stub_get(%r(/user/1)).to_return(body: fixture('user.1.json'))
-        client.user(1)
-        expect(request).to have_been_made
+        stub_get(%r{/user/1}).to_return(body: fixture('user.1.json'))
+        user = client.user(1)
+        expect(user.id).to eq 1
       end
     end
   end
 
   describe "#org" do
-    before do
-      stub_get(/example.com/).to_return(body: fixture('org.my.json'))
-    end
-
     context "when ID is not passed" do
       it "should return my org" do
-        expect(client.org).to be_a Crashplan::Org
+        stub_get(%r{/org/my}).to_return(body: fixture('org.my.json'))
+        org = client.org
+        expect(org.id).to eq 1
       end
     end
 
     context "when ID is passed in" do
       it "should return specific org" do
-        request = stub_get(%r(/org/1)).to_return(body: fixture("org.1.json"))
-        client.org(1)
-        expect(request).to have_been_made
+        stub_get(%r{/org/1}).to_return(body: fixture("org.1.json"))
+        org = client.org(1)
+        expect(org.id).to eq 1
       end
     end
   end

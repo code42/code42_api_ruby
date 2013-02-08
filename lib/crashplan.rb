6bd62d4056b7752c883e2be9857676c789404e58
Nick Colgan <nick.colgan@code42.com>
2013-02-08 12:36:16 -0600

    Be able to use singleton client.

--- lib/crashplan.rb
+++ lib/crashplan.rb
@@ -16,5 +16,24 @@ require "crashplan/token_validation"
 require "crashplan/user_role_collection"
 
 module Crashplan
-  # Your code goes here...
+  class << self
+    def client
+      @client ||= Crashplan::Client.new
+    end
+
+    def configure
+      yield self.settings
+      self
+    end
+
+    def respond_to_missing?(method_name, include_private=false); client.respond_to?(method_name, include_private); end if RUBY_VERSION >= "1.9"
+    def respond_to?(method_name, include_private=false); client.respond_to?(method_name, include_private) || super; end if RUBY_VERSION < "1.9"
+
+    private
+
+    def method_missing(method_name, *args, &block)
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
+    end
+  end
 end

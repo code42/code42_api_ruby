590a577f5303594d163548fd50f0c3bd287cf908
Melissa Voegeli <melissa@code42.com>
2013-04-11 15:16:31 -0500

    singles objects can have arrays of other objects in their responses

--- lib/crashplan/attribute_serializer.rb
+++ lib/crashplan/attribute_serializer.rb
@@ -53,7 +53,9 @@ module Crashplan
 
     def deserialize_value(key, value)
       if klass = attribute_for(key).try(:as)
-        if klass.respond_to?(:from_response)
+        if klass.respond_to?(:collection_from_response)
+          klass.collection_from_response(value)
+        elsif klass.respond_to?(:from_response)
           klass.from_response(value)
         elsif klass.respond_to?(:parse)
           klass.parse(value)

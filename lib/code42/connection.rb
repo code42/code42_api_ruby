0dd4f3003e6625d8b53fd540ba599b0ca672afb2
Aaron Ackerman <aaron.ackerman@code42.com>
2014-05-08 08:43:23 -0500

    Use ActiveSupport notifications to instrument requests and responses

--- lib/code42/connection.rb
+++ lib/code42/connection.rb
@@ -59,6 +59,11 @@ module Code42
     def make_request(method, *args)
       begin
         response = self.send(method, *args)
+        ActiveSupport::Notifications.instrument('code42.request', {
+          method:   method,
+          args:     args,
+          response: response
+        })
       rescue Faraday::Error::ConnectionFailed
         raise Code42::Error::ConnectionFailed
       end

477ca2747cc7ea05563b86848cd4bae79ba88cec
Mark Eyberg <mark.eyberg@code42.com>
2015-04-30 16:49:10 -0500

    Enabled support for searching for destinations by name

--- lib/code42/api/destination.rb
+++ lib/code42/api/destination.rb
@@ -1,16 +1,12 @@
 module Code42
   module API
     module Destination
-      def destinations
-        objects_from_response(Code42::Destination, :get, 'destinations', key: 'destinations')
+      def destinations(name = nil)
+        objects_from_response(Code42::Destination, :get, "destinations#{"?name=#{name}" if name}", key: 'destinations')
       end
 
       def destination(id)
-        if id.is_a? Numeric
-          object_from_response(Code42::Destination, :get, "destination/#{id}")
-        else
-          objects_from_response(Code42::Destination, :get, "destination?name=#{id}", key: 'destinations')
-        end
+        object_from_response(Code42::Destination, :get, "destination/#{id}")
       end
 
       def create_destination(attrs)
